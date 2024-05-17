dd80e25e0b49c861aae4efa824d2635a94d2d703
jospin6 <jospinndagano1@gmail.com>
2024-05-17 15:54:35 +0200

    Remove the blue background color

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,8 +1,8 @@
 import { SearchBar } from "./SearchBar"
 
 export const Navbar = () => {
-    return <div className="w-full h-[40px] flex items-center justify-between px-6 bg-blue-800">
-        <div>title</div>
+    return <div className="w-full h-[40px] flex items-center justify-between px-6">
+        <div className="text-xl text-white">title</div>
         <SearchBar/>
     </div>
 }
