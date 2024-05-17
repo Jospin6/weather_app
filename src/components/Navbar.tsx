ad1768d571383338c31076d24ba45a8e4587c74f
jospin6 <jospinndagano1@gmail.com>
2024-05-17 15:59:49 +0200

    Add the app title

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,7 +2,7 @@ import { SearchBar } from "./SearchBar"
 
 export const Navbar = () => {
     return <div className="w-full h-[40px] flex items-center justify-between px-6">
-        <div className="text-xl text-white">title</div>
+        <div className="text-xl text-white">Weather</div>
         <SearchBar/>
     </div>
 }
