06e97900898b3300e99ac64f304b3753bd506dd9
jospin6 <jospinndagano1@gmail.com>
2024-05-17 16:43:44 +0200

    Add the conditional rendering to check if there is or not error

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 import { useDispatch, useSelector } from "react-redux"
 import { Navbar } from "./components/Navbar"
 import { WeatherInfos } from "./components/WeatherInfos"
-import {getWeather, getquery, image} from './helpers/index'
+import {getWeather, getquery, image,getEror} from './helpers/index'
 import { useEffect } from "react"
 import { AppDispatch } from './features/store'
 import { fetchWeather } from './features/weatherSlice'
+import { ErrorMessage } from "./components/ErrorMessage"
 
 function App() {
   const weather = useSelector(getWeather)
+  const error = useSelector(getEror)
   const dispatch = useDispatch<AppDispatch>()
   const query = useSelector(getquery)
   useEffect(() => {
@@ -24,7 +26,10 @@ const backgroundImage = {
     <>
       <div className="w-full h-[100vh]" style={backgroundImage}>
           <Navbar/>
-          <WeatherInfos weather={weather}/>
+          {!error 
+            ? <WeatherInfos weather={weather}/> 
+            : <ErrorMessage error={error}/>}
+          
       </div>
     </>
   )
