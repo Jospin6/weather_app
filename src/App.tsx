c3df7f44dc61c454dd295fc592289430ae40be6e
jospin6 <jospinndagano1@gmail.com>
2024-05-17 11:05:33 +0200

    Design the search bar

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
+import { SearchBar } from "./components/SearchBar"
+
 function App() {
 
   return (
     <>
-      <div className="text-red-500">Hello world jn</div>
+      <div className="w-full h-[100vh] bg-red-400 flex items-center justify-center">
+          <div className="w-[400px] h-[500px] bg-blue-400">
+            <SearchBar/>
+          </div>
+      </div>
     </>
   )
 }
--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,7 +1,8 @@
-
+import search from '../assets/images/search.png'
 
 export const SearchBar = () => {
-    return <div>
-        
+    return <div className='flex bg-white h-[25px] rounded-lg'>
+        <input type="text" placeholder="city name" className='mx-[5px] w-[90%] outline-none'/>
+        <img src={search} alt="search" width="20" height="20" className='object-fit py-[2px]' />
     </div>
 }
