9fbadaaa3ff2077addd9caa864949e1c53ce16e1
jospin6 <jospinndagano1@gmail.com>
2024-05-17 12:18:19 +0200

    Remove unused backgrounds

--- src/components/WeatherInfos.tsx
+++ src/components/WeatherInfos.tsx
@@ -1,7 +1,7 @@
 
 export const WeatherInfos = () => {
     return <div className="grid grid-cols-4 gap-6 h-[94vh]">
-        <div className="col-span-2 relative bg-blue-600">
+        <div className="col-span-2 relative">
             <div className="absolute left-[100px] bottom-[100px] 
             blackBgWithOpacity rounded-lg h-[auto] min-w-[200px] w-auto p-2 text-white">
                 <div className="flex items-top ">
@@ -12,7 +12,7 @@ export const WeatherInfos = () => {
                 <div className="text-[17px] font-[200]">Today sun 28 may</div>
             </div>
         </div>
-        <div className="col-span-2 bg-yellow-600 flex justify-center pt-4">
+        <div className="col-span-2 flex justify-center pt-4">
             <div>
                 <div className="rounded-lg h-[200px] w-[400px] bg-gray-400 mb-4"></div>
                 <div className="rounded-lg h-[200px] w-[400px] bg-gray-400"></div>
