e0efd835405cd9c1f23dfc3fe8976f12fbeb6643
jospin6 <jospinndagano1@gmail.com>
2024-05-18 13:23:06 +0200

    Add media queries to the navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,7 +2,7 @@ import { SearchBar } from "./SearchBar"
 
 export const Navbar = () => {
     return <div className="w-full h-[50px] flex items-center justify-between px-6">
-        <div className="text-xl text-white">Weather</div>
+        <div className="md:text-xl text-[15px] text-white">Weather</div>
         <SearchBar/>
     </div>
 }
