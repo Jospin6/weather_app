114e4dff7fefb9f291397c6d865eececdccedde7
jospin6 <jospinndagano1@gmail.com>
2024-05-19 17:49:59 +0200

    Refactor code in ErrorMessage.tsx

--- src/components/ErrorMessage.tsx
+++ src/components/ErrorMessage.tsx
@@ -1,7 +1,8 @@
 
 export const ErrorMessage = () => {
     return <div className="h-[92vh] flex items-center justify-center">
-        <div className="rounded-lg bg-red-700 w-[300px] h-[200px] text-red-300 flex items-center justify-center ">
+        <div className="rounded-lg bg-red-700 w-[300px] h-[200px] 
+        text-red-300 flex items-center justify-center ">
             <span>Please enter a wright city name!</span>
         </div>
     </div>
